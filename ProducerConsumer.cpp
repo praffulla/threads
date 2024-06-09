2e8158e20acfafd4e6801fa631c48ae30f2318dc
praffulla <praffullapandey.05@gmail.com>
2024-06-09 13:29:24 +0530

    Create ProducerConsumer.cpp

---
+++ ProducerConsumer.cpp
@@ -0,0 +1,62 @@
+Always write down all the constraints before solving any thread problems:
+Here are three conditions:
+1. Mutual exclusion while accesing buffer. (R-W)
+2. Producer can't produce more if buffer is full;
+3. Consumer can't consume more if buffer is empty;
+
+#include <iostream>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+mutex m;
+condition_variable cv;
+deque<int> buffer;
+int maxBuffSize = 50;
+
+
+bool isSomeSpace() {
+	if (buffer.size() < maxBuffSize )
+		return true;
+	return false;
+}
+
+bool isSomeItems() {
+        if (!buffer.empty())
+                return true;
+        return false;
+}
+
+void producer(int val) {
+   while(val) {
+	unique_lock<mutex> ul(m);
+	cv.wait(ul, isSomeSpace);
+	buffer.push_back(val);
+	cout << "Producer:" << val << endl;
+	val--;
+	cv.notify_one();
+   }
+  
+}
+
+void consumer() {
+   while(true) {
+	unique_lock<mutex> ul(m);
+	cv.wait(ul, isSomeItems);
+	int item = buffer.back();
+	buffer.pop_back();
+	cout << "Consumer: " << item << endl;
+	cv.notify_one();
+}
+
+int main () {
+
+	thread t1(producer, 100);
+	thread t2(consumer);
+
+	t1.join();
+	t2.join();
+}
